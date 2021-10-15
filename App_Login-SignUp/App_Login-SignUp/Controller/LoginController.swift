151daf1b76d7832bf87d6a884089a219747c432f
Kaique <kaique.lopes3271@gmail.com>
2021-10-14 23:30:32 -0300

    IconImage and Assets

--- App_Login-SignUp/App_Login-SignUp/Controller/LoginController.swift
+++ App_Login-SignUp/App_Login-SignUp/Controller/LoginController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class LoginController: UIViewController {
+
+    let imageFirebase = UIImage(named: "firebase-logo")
+    let iconImage: UIImageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,11 +19,20 @@ class LoginController: UIViewController {
     
     // MARK: - Create Gradient Color
     func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
         let gradient = CAGradientLayer()
         gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
-        gradient.locations = [0 , 1]
+        gradient.locations = [0 , 1.5]
         view.layer.addSublayer(gradient)
         gradient.frame = view.frame
+        
+        iconImage.image = imageFirebase
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 120, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 25)
     }
 }
 
