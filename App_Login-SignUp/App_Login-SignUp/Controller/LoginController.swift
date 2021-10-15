3a332a1cdb6044b771792996b9669aea677a300f
Kaique <kaique.lopes3271@gmail.com>
2021-10-15 16:12:48 -0300

    ForgotPassword

--- App_Login-SignUp/App_Login-SignUp/Controller/LoginController.swift
+++ App_Login-SignUp/App_Login-SignUp/Controller/LoginController.swift
@@ -23,8 +23,25 @@ class LoginController: UIViewController {
     
     private let loginButton: AuthButton = {
         let button = AuthButton(type: .system)
-        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Create Button Forgot Password
+    
+    private let forgotPassword: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let atts : [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.8),
+                                                    .font: UIFont.systemFont(ofSize: 15)]
+        let attributedTitle = NSMutableAttributedString(string: "Esqueci minha senha! ", attributes: atts)
+        
+        let boldAtts : [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.8),
+                                                        .font: UIFont.boldSystemFont(ofSize: 15)]
+        attributedTitle.append(NSAttributedString(string: " Lembrar Senha", attributes: boldAtts))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(handleForgotPassword), for: .touchUpInside)
         return button
     }()
     
@@ -33,10 +50,14 @@ class LoginController: UIViewController {
         configureUI()
     }
     
+    @objc func handleForgotPassword() {
+        print("DEBUG--HandleForgotPassword")
+    }
+    
     @objc func handleLogin() {
         print("DEBUG-- Handle")
     }
-
+    
     func configureUI() {
         // MARK: - Func Gradient Colors
         navigationController?.navigationBar.isHidden = true
@@ -55,7 +76,7 @@ class LoginController: UIViewController {
         iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 25)
         
         // MARK: - StackView with TextFields
-        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, forgotPassword])
         stack.axis = .vertical
         stack.spacing = 20
         view.addSubview(stack)
