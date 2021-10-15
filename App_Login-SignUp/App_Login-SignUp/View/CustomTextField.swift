d5e39559114c39eab1c55091cda94ddc43f4856a
Kaique <kaique.lopes3271@gmail.com>
2021-10-15 12:01:31 -0300

    CustomTextField

---
+++ App_Login-SignUp/App_Login-SignUp/View/CustomTextField.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTextField.swift
+//  App_Login-SignUp
+//
+//  Created by Kaique Lopes de Oliveira on 15/10/21.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    init(placeholder: String){
+        super.init(frame: .zero)
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        leftView = spacer
+        leftViewMode = .always
+        
+        textColor = .white
+        keyboardAppearance = .dark
+        backgroundColor = UIColor(white: 1, alpha: 0.1)
+        setHeight(height: 50)
+        attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [.foregroundColor:
+                                                                                                UIColor(white: 1, alpha: 0.5)])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
